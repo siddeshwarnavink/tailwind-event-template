e2bdd4ca00525d908d62d9c85a19c03874ad3f93
Dylan Hassinger <d@dylanized.com>
2019-05-25 17:53:43 -0400

    added keynamed colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,24 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
-    extend: {},
-    colors: {
-      primary: {
-        lt: '#5c6ac4',
-        md: '#5c6ac4',
-        dk: '#5c6ac4',
-      },
-      secondary: {
-        lt: '#5c6ac4',
-        md: '#5c6ac4',
-        dk: '#5c6ac4',
-      },
-      tertiary: {
-        lt: '#5c6ac4',
-        md: '#5c6ac4',
-        dk: '#5c6ac4',
+    extend: {
+      colors: {
+        primary: {
+          lt: colors.blue[300],
+          md: colors.blue[500],
+          dk: colors.blue[700],
+        },
+        secondary: {
+          lt: colors.red[300],
+          md: colors.red[500],
+          dk: colors.red[700],
+        },
+        tertiary: {
+          lt: colors.purple[300],
+          md: colors.purple[500],
+          dk: colors.purple[700],
+        },
       },
     },
   },
