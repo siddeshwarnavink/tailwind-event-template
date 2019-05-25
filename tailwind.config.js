fd049cd359479585c1c4e82831f90a8f756c59da
Dylan Hassinger <d@dylanized.com>
2019-05-25 17:44:34 -0400

    stubbed out primary secondary tertiary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
 module.exports = {
   theme: {
     extend: {},
+    colors: {
+      primary: {
+        lt: '#5c6ac4',
+        md: '#5c6ac4',
+        dk: '#5c6ac4',
+      },
+      secondary: {
+        lt: '#5c6ac4',
+        md: '#5c6ac4',
+        dk: '#5c6ac4',
+      },
+      tertiary: {
+        lt: '#5c6ac4',
+        md: '#5c6ac4',
+        dk: '#5c6ac4',
+      },
+    },
   },
   variants: {},
   plugins: [],
