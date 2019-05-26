ed42d00683849e5ed85c24a86d24ef79252fd9f4
Dylan Hassinger <d@dylanized.com>
2019-05-26 08:16:27 -0400

    added screen overrides

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ const { colors } = require('tailwindcss/defaultTheme')
 
 module.exports = {
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     fontFamily: {
       sans: [
         '-apple-system',
